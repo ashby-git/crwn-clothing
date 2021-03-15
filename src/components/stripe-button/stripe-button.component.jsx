30096dea5e43687016c14cd8daad6f5e816f4e1a
Ashby McFall <ashbymcfall@Ashbys-MacBook-Pro.local>
2021-03-15 17:35:07 +0900

    website version with scss files

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51IRXjdKgx8b9LifUvVhohWHqnwInjwVQOeOCiR7oTk2ypYuJEt0kPr2vXpktiiyZcUt0nvxalGjE1DJjE00fp2rK00pyGRvQQA';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
